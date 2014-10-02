636906aa3b3cf5dea040994a6894b0433eba8244
etna-scorpio <etna.scorpio@gmail.com>
2014-10-02 15:50:10 +0300

    fx

--- js/common.js
+++ js/common.js
@@ -3,6 +3,11 @@ head.ready(function() {
 	// $(document).on("click", function(){
 	// 	$(".js-popup").hide();
 	// });
-
+	///menu-mob
+	$('.header__btn').on('click',function(){
+	    $(this).parent('.header').find('.header__menu').slideToggle('400');
+	    $(this).toggleClass('is-open');
+	});
+	///
 	console.log($('body').html());
 });
